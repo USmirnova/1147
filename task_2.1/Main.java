7603ff0be3ed560ca8a93fe9e075cab6e2518b9f
USmirnova <90193806+USmirnova@users.noreply.github.com>
2021-09-08 13:46:11 +0300

    extra variable was removed

--- task_2.1/Main.java
+++ task_2.1/Main.java
@@ -20,7 +20,6 @@
 public class Main {
     public static void main(String[] args) {
       String arr[] = {"Мама", "Мыла", "Раму"};
-      String arrNew[] = {"", "", ""};
       int count = 0;
       for (int i=0; i<arr.length; i++) {
         for (int j=0; j<arr.length; j++) {
