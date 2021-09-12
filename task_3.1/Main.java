d49d133a82d7df7a2f3379120f80e3f2a70af87e
USmirnova <90193806+USmirnova@users.noreply.github.com>
2021-09-12 15:59:58 +0300

    a more adequate solution
    одна ф-я принимает 2 числа и сравнивает их
    вторая ф-я принимает 3 числа и с помощью первой ф-и сравнивает (1 и 2) и (2 и 3) числа.

--- task_3.1/Main.java
+++ task_3.1/Main.java
@@ -6,49 +6,19 @@
 •	Метод main должен выводить на экран результат работы метода min. Каждый раз с новой строки.
 •	Метод min должен возвращать минимальное значение из чисел a, b и с.
 */
-
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(min(1, 2, 3));    // 1
-        System.out.println(min(-1, -2, -3)); // -3
-        System.out.println(min(3, 5, 3));    // 3
-        System.out.println(min(5, 5, 10));   // 5
-        /*
-        // --- для проверки всех условий
-        System.out.println(min(1, 1, 1)); // 1 //все равны
-
-        System.out.println(min(1, 1, 9)); // 1 // равны 2 из 3
-        System.out.println(min(9, 9, 1)); // 1
-        System.out.println(min(1, 9, 9)); // 1
-        System.out.println(min(9, 9, 1)); // 1
-        System.out.println(min(1, 9, 1)); // 1
-        System.out.println(min(9, 1, 9)); // 1
-
-        System.out.println(min(1, 2, 3)); // 1 // все не равны
-        System.out.println(min(3, 1, 2)); // 1 // все не равны
-        System.out.println(min(2, 3, 1)); // 1 // все не равны
-         */
+        System.out.println(min3n(1, 2, 3)); // 1
+        System.out.println(min3n(-1, -2, -3)); //-3
+        System.out.println(min3n(3, 5, 3)); //3
+        System.out.println(min3n(5, 5, 10)); // 5
     }
-
-    public static int min(int a, int b, int c) {
-        int error = (int) (-1/0.0); // проверка на неучтенное условие
-
-        if (a==b && b==c) return a; // все равны
-        else if (a==b && a < c ) return a;
-        else if (a==b && a > c ) return c;
-        else if (b==c && a < c ) return a;
-        else if (b==c && a > c ) return c;
-        else if (a==c && a < b ) return a;
-        else if (a==c && a > b ) return b;
-        else if (a!=b && b != c && a!=c) { // все не равны
-            if (a < b && a < c) return a;
-            else if (b < a && b < c) return b;
-            else if (c < a && c < b) return c;
-            else return error; // проверка на неучтенное условие
-        }
-        else return error; // проверка на неучтенное условие
+    public static int min3n(int a, int b, int c) {
+        return min2n(min2n(a,b),min2n(b,c));
     }
-
+    public static int min2n(int a, int b) {
+        if (a<b) return a;
+        else return b; // (a>b) || (a==b)
+    }
 
 }
