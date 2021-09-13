e19d2fa1d8031a94dc9e498e5e4e0c61f78db4ae
USmirnova <90193806+USmirnova@users.noreply.github.com>
2021-09-13 23:05:28 +0300

    Create Animal.java

---
+++ task_4.5/Animal.java
@@ -0,0 +1,17 @@
+public class Animal {
+	boolean vegetarian; // травоядное
+	String speak; // звуки издает
+	byte paws; // лапы
+
+	public Animal(boolean veg, String speak, byte paws) {
+		this.vegetarian = veg;
+		this.speak = speak;
+		this.paws = paws;
+	}
+	boolean isVegetarian() {return this.vegetarian;}
+	String getSpeak() {return this.speak;}
+	byte getPaws() {return this.paws;}
+	void eat() { // общий для всех метод
+		System.out.println("Я кушаю");
+	}
+}
