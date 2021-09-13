519029d6fdc607852d066459be71fddc9496c497
USmirnova <90193806+USmirnova@users.noreply.github.com>
2021-09-13 23:06:44 +0300

    Create Pegasus.java

---
+++ task_4.5/Pegasus.java
@@ -0,0 +1,7 @@
+public class Pegasus extends Horse {
+	public Pegasus(boolean veg, String speak, byte paws, String breed, String color) {
+		super(veg, speak, paws, breed, color);
+	}
+
+	public void fly(){ System.out.println("Игого, я полетел(а)"); }
+}
