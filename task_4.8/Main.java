a12af7d4a49c6487564c5b5b5056886e252e3b7f
USmirnova <90193806+USmirnova@users.noreply.github.com>
2021-09-16 11:22:11 +0300

    Create Main.java

---
+++ task_4.8/Main.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+/*
+ *  ***Гостиница***
+ *  1) Гостиница
+ *  2) Номера (кол-во мест(1-3), сан.узел(есть/нет), питаение(есть/нет), wifi(есть/нет), свободен/занят)
+ *
+ *   *Задание для лабораторной работы №4*
+ *   Освободить комнату
+ *   Вывести свойства комнаты
+ *   Показать комнаты с WiFi
+ *   Показать комнаты с WC
+ *   Показать комнаты с Eat
+ *   Показать комнаты по кол-ву спальных мест
+ *   Отобразить список команд
+ * */
+public class Main {
+public static void main(String[] args) {
+	Room[] rooms = {//
+			new Room((byte) 1,false,false,true,(byte) 11),
+			new Room((byte) 2,true,true,false,(byte) 12),
+			new Room((byte) 1,false,true,true,(byte) 13),
+			new Room((byte) 3,true,false,false,(byte) 14),
+			new Room((byte) 2,false,false,false,(byte) 15),
+			new Room((byte) 3,true,true,true,(byte) 16),
+			new Room((byte) 1,false,true,false,(byte) 17),
+			new Room((byte) 3,true,true,false,(byte) 18),
+			new Room((byte) 1,false,false,true,(byte) 19),
+	};
+	Hotel hotel = new Hotel(rooms);
+	Scanner scanner = new Scanner(System.in);
+	String command;
+	while (true){
+		System.out.println("Введите команду:");
+		command = scanner.nextLine();
+		if(command.equals("getFreeRooms")){
+			hotel.getFreeRooms();
+		}
+		else if (command.equals("getReservedRooms")) {
+			hotel.getReservedRooms();
+		}
+		else if (command.equals("selectOfParam")) {
+			System.out.println("Введите параметры комнаты через пробел, например: quantity:3 wc eat wifi isfree");
+			String param = scanner.nextLine();
+			hotel.selectOfParam(param);
+		}
+		else if(command.equals("reserveRoom")){
+			System.out.print("Введите номер комнаты для бронирования: ");
+			byte roomNumber = (byte) scanner.nextInt();
+			hotel.reserveRoom(roomNumber);
+		}
+		else if (command.equals("checkoutRoom")) {
+			System.out.print("Введите номер освобождаемой комнаты: ");
+			byte roomNumber = (byte) scanner.nextInt();
+			hotel.checkoutRoom(roomNumber);
+		}
+		else if (command.equals("getRoom")) {
+			System.out.print("Введите номер комнаты: ");
+			byte roomNumber = (byte) scanner.nextInt();
+			hotel.getRoom(roomNumber);
+		}
+		else if (command.equals("help")){
+			System.out.print("------------------\n"+
+							"getFreeRooms - свободные комнаты;\n"+
+							"getReservedRooms - зарезервированные комнаты;\n"+
+							"selectOfParam - подбор комнат по различным параметрам, например: quantity:3 wc eat wifi isfree ;\n"+
+							"getRoom - свойства комнаты;\n"+
+							"reserveRoom - бронирование комнаты;\n"+
+							"checkoutRoom - освобождение комнаты;\n"+
+							"help - список команд;\n"+
+							"exit - выход;\n"+"------------------\n");
+		}
+		else if (command.equals("exit")){
+			break;
+		}
+	}
+
+}
+}
